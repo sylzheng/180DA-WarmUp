f56388f1fec0b04ed94b03f60ebe6354db52858d
sylzheng <44931220+sylzheng@users.noreply.github.com>
2021-10-15 22:28:13 -0700

    Lab 3: MQTT subscriber

---
+++ subscriber.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: "+str(rc))
+    client.subscribe("ece180d/test", qos=1)
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print('Unexpected Disconnect')
+    else:
+        print('Expected Disconnect')
+
+def on_message(client, userdata, message):
+    print('Received message: "' + str(message.payload) + '" on topic "' +
+            message.topic + '" with QoS ' + str(message.qos))
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+
+client.connect_async('mqtt.eclipseprojects.io')
+
+client.loop_start()
+
+while True:
+    pass
+
+client.loop_stop()
+client.disconnect()
+
